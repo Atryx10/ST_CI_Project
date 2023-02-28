bd3592f3dae59f91df6906cb130be13b543ad238
Atryx10 <emhd17@student.bth.se>
2023-02-28 17:00:33 +0100

    Basic Hello World Program to get a feel for the language and Eclipse IDE

---
+++ src/ST_CI_Project/ST_CI_Main.java
@@ -0,0 +1,10 @@
+package ST_CI_Project;
+
+public class ST_CI_Main
+{
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+}
