58bb4cd2e95ee9968d2c61ff27c902417ea003bc
Caitanyakotla <kotlakrishna520@gmail.com>
2023-03-12 18:02:38 +0100

    added additional

--- src/main/java/calc/Calculator.java
+++ src/main/java/calc/Calculator.java
@@ -40,11 +40,6 @@ public class Calculator {
 
     }
 
-    public static int add(int a, int b)
-    {
-        return a + b;
-    }
-
     public static double divide(double a, double b)
     {
         return a/b;
